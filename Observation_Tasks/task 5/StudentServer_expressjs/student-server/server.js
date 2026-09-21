570a1d5444c5acb0b2140f94cee13e9ec7c1d10a
gedelakavya28 <gedelakavya28@gmail.com>
2026-09-21 22:47:32 +0530

    modified

--- Observation_Tasks/task 5/StudentServer_expressjs/student-server/server.js
+++ Observation_Tasks/task 5/StudentServer_expressjs/student-server/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
-
 const app = express();
-
 const port = 3000;
-
 // Student data
 const students = [
     {
@@ -32,22 +29,18 @@ const students = [
         course: "IT"
     }
 ];
-
 // Home route
 app.get('/', (req, res) => {
     res.send('Welcome to Student Server!');
 });
-
 // Students route
 app.get('/students', (req, res) => {
     res.json(students);
 });
-
 // About route
 app.get('/about', (req, res) => {
     res.send('This application manages student information using Express.js.');
 });
-
 // Start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
